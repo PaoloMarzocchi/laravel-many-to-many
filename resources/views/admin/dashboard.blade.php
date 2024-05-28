cb6f8f75e4217e9c1020c5cf449c1464086dae57
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-28 18:05:05 +0200

    edit admin dashboard layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,31 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Dashboard') }}
-        </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
+    <section class="admin_section">
+        <div class="container">
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+            <h2 class="fs-4 title_section py-4 mt-0">
+                {{ __('Dashboard') }}
+            </h2>
+            <div class="row vh-100 justify-content-center">
+                <div class="col pt-5">
+                    <div class="card mt-5">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                        {{ __('You are logged in!') }}
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
                 </div>
             </div>
+
+
         </div>
-    </div>
+    </section>
 @endsection
